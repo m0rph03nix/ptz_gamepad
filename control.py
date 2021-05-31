4ba746afe81a5320e1eae96e4669d783b55fd285
Raphael LEBER <raphael.leber@cpe.fr>
2021-05-31 16:29:15 +0200

    v0 full working control.py

--- control.py
+++ control.py
@@ -5,73 +5,219 @@ from __future__ import print_function
 import time
 from inputs import get_gamepad
 
-HIGH_TH = 127 + 8
-LOW_TH = 127 - 8
+import requests
+
+import urllib.request
+
+IP = "192.168.1.88"
+
+GAMEPAD = "XBOX"
+
+""" HIGH_TH = 127 + 8
+LOW_TH = 127 - 8 """
+
+CAM_MAX = 20
+JOY_MAX = 32767
+JOY_MIN = -32767
+HIGH_TH = 6000
+LOW_TH = -6000
+JOY_CAM_RATIO = CAM_MAX / (JOY_MAX - HIGH_TH )
+
 
 class PTZ():
 
     def __init__(self):
         self.pan = 0
+        self.pan_ori = "left"
+        self.tilt = 0
+        self.tilt_ori = "up"
+        self.zoom = 0
+        self.zoom_ori = "zoomin"
+        self.Preset = 0
         self.absolute_switch = {
-            "ABS_RX" : self.update_pan,
-            "ABS_RZ" : self.update_tilt,
-
-            "ABS_Y" : self.update_zoom
+            "ABS_PAN" : self.update_pan,
+            "ABS_TILT" : self.update_tilt,
+            "ABS_ZOOM" : self.update_zoom,
+            "ABS_FOCUS_IN" : self.update_focus_in,
+            "ABS_FOCUS_OUT" : self.update_focus_out      
         }
+
         self.btn_switch = {
-            "BTN_THUMB" : self.manage_preset,
-            "BTN_THUMB2" : self.manage_preset,
-            "BTN_TOP" : self.manage_preset,
-            "BTN_TRIGGER" : self.manage_preset,
-
-            "BTN_BASE" : self.manage_common_btn,
-            "BTN_BASE2" : self.manage_common_btn,
-
+            "BTN_PRESET0" : self.manage_preset,
+            "BTN_PRESET1" : self.manage_preset,
+            "BTN_PRESET2" : self.manage_preset,
+            "BTN_PRESET3" : self.manage_preset,
         }        
 
+        self.map_XBOX = {
+            "BTN_NORTH" : "BTN_PRESET0",
+            "BTN_SOUTH" : "BTN_PRESET1",
+            "BTN_EAST" : "BTN_PRESET2",
+            "BTN_WEST" : "BTN_PRESET3",
 
+            "ABS_RX" : "ABS_PAN",
+            "ABS_RY" : "ABS_TILT",
+            "ABS_Y" : "ABS_ZOOM",    
+            "ABS_RZ" : "ABS_FOCUS_IN",
+            "ABS_Z" : "ABS_FOCUS_OUT"       
+        }
+
+        self.map_CPE = {
+            "BTN_THUMB" : "BTN_PRESET0",
+            "BTN_THUMB" : "BTN_PRESET1",
+            "BTN_TOP"   : "BTN_PRESET2",
+            "BTN_TRIGGER" : "BTN_PRESET3",
+
+            "ABS_RX" : "ABS_PAN",
+            "ABS_RZ" : "ABS_TILT",
+            "ABS_Y" : "ABS_ZOOM"            
+        }        
+
+        self.map = {
+            "XBOX" : self.map_XBOX,
+            "CPE"  : self.map_CPE
+        }
+
+        self.ori_pan = {
+            "+" : "right",
+            "-" : "left"
+        }
+
+        self.ori_tilt = {
+            "+" : "up",
+            "-" : "down"
+        }   
+
+        self.ori_zoom = {
+            "+" : "zoomin",
+            "-" : "zoomout"
+        } 
+
+        self.ori_focus = {
+            "+" : "focusin",
+            "-" : "focusout"
+        }                       
+     
+        
+
+    def update_axis_cmd(self, val):
+        if(val > HIGH_TH ):
+            axis = min((val - HIGH_TH) * JOY_CAM_RATIO, CAM_MAX)
+            axis_ori = '+'
+        elif(val < LOW_TH ):
+            axis = min(abs(val - LOW_TH) * JOY_CAM_RATIO, CAM_MAX)
+            axis_ori = '-'
+        else:
+            axis = 0  
+            axis_ori = '+'
+
+        return int(axis), axis_ori
 
 
 
     def update_pan(self, val):
-        print("pan")
+        self.pan, ori = self.update_axis_cmd(val)  
+        self.pan_ori = self.ori_pan[ori]
         
-        if(val > HIGH_TH ):
-            self.pan = (val - HIGH_TH) / 6.4
-        elif(val < LOW_TH ):
-            self.pan = -(HIGH_TH - val) / 6.4
-        else:
-            self.pan = 0
+        if self.pan != 0 :
+            p = requests.get("http://"+ IP + "/cgi-bin/ptzctrl.cgi?ptzcmd&{0}&{1}&{2}".format(self.pan_ori, self.pan, self.tilt) )
+            print("pan : ", self.pan)    
+        
+        elif self.tilt == 0:
+            p = requests.get("http://"+ IP + "/cgi-bin/ptzctrl.cgi?ptzcmd&{0}&{1}&{2}".format("ptzstop", self.pan, self.tilt) )
+
+
 
     def update_tilt(self, val):
-        print("tilt")
-        pass
+        self.tilt, ori = self.update_axis_cmd(val)  
+        self.tilt_ori = self.ori_tilt[ori]   
+        
+        if self.tilt != 0 :
+            p = requests.get("http://"+ IP + "/cgi-bin/ptzctrl.cgi?ptzcmd&{0}&{1}&{2}".format(self.tilt_ori, self.pan, self.tilt) )
+            print("tilt : ", self.tilt)
+        
+        elif self.pan == 0:
+            p = requests.get("http://"+ IP + "/cgi-bin/ptzctrl.cgi?ptzcmd&{0}&{1}&{2}".format("ptzstop", self.pan, self.tilt) )
+
 
     def update_zoom(self, val):
-        print("zoom")
-        pass    
+        self.zoom, ori = self.update_axis_cmd(val)  
+        self.zoom_ori = self.ori_zoom[ori]    
+        
+        if self.zoom != 0 :
+            p = requests.get("http://"+ IP + "/cgi-bin/ptzctrl.cgi?ptzcmd&{0}&{1}".format(self.zoom_ori, self.zoom) )
+            print("zoom : ", self.zoom)  
+        else:
+            p = requests.get("http://"+ IP + "/cgi-bin/ptzctrl.cgi?ptzcmd&{0}&{1}".format("zoomstop", self.zoom) )
 
-    def update_focus(self, val):
-        print("focus")
-        pass   
+
+    def update_focus_in(self, val):
+
+        if(abs(val) > 20 ):
+            axis = min((val - 20) * (20/(255-20)), 20)
+            axis_ori = '+'
+        else:
+            axis = 0  
+            axis_ori = '+'
+
+        self.focus = int(axis)
+        self.focus_ori = self.ori_focus[axis_ori]    
+        
+        if self.focus != 0 :
+            p = requests.get("http://"+ IP + "/cgi-bin/ptzctrl.cgi?ptzcmd&{0}&{1}".format(self.focus_ori, self.focus) )
+            print("focus : ", self.focus)  
+        else:
+            p = requests.get("http://"+ IP + "/cgi-bin/ptzctrl.cgi?ptzcmd&{0}&{1}".format("focusstop", self.focus) )
+            print("stop focus : ", self.focus)    
+
+
+    def update_focus_out(self, val):
+
+        if(abs(val) > 20 ):
+            axis = min((val - 20) * (20/(255-20)), 20)
+            axis_ori = '-'
+        else:
+            axis = 0  
+            axis_ori = '-'
+
+        self.focus = int(axis)
+        self.focus_ori = self.ori_focus[axis_ori]    
+        
+        if self.focus != 0 :
+            p = requests.get("http://"+ IP + "/cgi-bin/ptzctrl.cgi?ptzcmd&{0}&{1}".format(self.focus_ori, self.focus) )
+            print("focus : ", self.focus)  
+        else:
+            p = requests.get("http://"+ IP + "/cgi-bin/ptzctrl.cgi?ptzcmd&{0}&{1}".format("focusstop", self.focus) )
+            print("stop focus : ", self.focus)     
 
 
 
     def manage_preset(self, btn, val):
         print("preset")
 
-        if(btn == "BTN_THUMB"):
-            if( val == 1):
-                self.BTN_THUMB_t0 = time.time()
+        if( val == 1):
+            self.BTN_t0 = time.time()
+        else:
+            BTN_t1 = time.time()
+            BTN_dt = BTN_t1 - self.BTN_t0
+            print(BTN_dt)
+            if (BTN_dt < 2):
+                print("short")
+                pos = "poscall"
             else:
-                BTN_THUMB_t1 = time.time()
-                BTN_THUMB_dt = BTN_THUMB_t1 - self.BTN_THUMB_t0
-                print(BTN_THUMB_dt)
-                if (BTN_THUMB_dt < 2.5):
-                    print("short")
-                else:
-                    print("long")
-        pass   
+                print("long")
+                pos = "posset"
+
+            if(btn == "BTN_PRESET0"):
+                self.Preset = 0
+            elif(btn == "BTN_PRESET1"):
+                self.Preset = 1
+            elif(btn == "BTN_PRESET2"):
+                self.Preset = 2
+            elif(btn == "BTN_PRESET3"):
+                self.Preset = 3  
+            p = requests.get("http://"+ IP + "/cgi-bin/ptzctrl.cgi?ptzcmd&" + pos + "&" + str(self.Preset))
+  
 
 
     def manage_common_btn(self, val):
@@ -87,13 +233,15 @@ class PTZ():
 
                 if(event.ev_type == "Absolute" ):
 
-                    if event.code in self.absolute_switch.keys():
-                        self.absolute_switch[event.code](event.state)
+                    if event.code in self.map[GAMEPAD].keys():
+                        self.absolute_switch[ self.map[GAMEPAD][event.code] ](event.state)
+
 
                 if(event.ev_type == "Key" ):
 
-                    if event.code in self.btn_switch.keys():
-                        self.btn_switch[event.code](event.code, event.state)
+                    if event.code in self.map[GAMEPAD].keys():
+                        self.btn_switch[ self.map[GAMEPAD][event.code] ](self.map[GAMEPAD][event.code], event.state)
+                        
 
 
 
